6de6f6f62b6377ad49b02f94dd5bf38308f61792
Sean A <1996.sean.alexander@gmail.com>
2019-06-01 19:35:40 -0700

    Implemented dynamic element set up on page (set color clue & underscore count to match & refresh to a random color, guess count down); laid out elements and did further styling

--- assets/javascript/background.js
+++ assets/javascript/background.js
@@ -1,43 +1,38 @@
 //pull main tag for background 
-var main = document.querySelector('main');
+let main = document.querySelector('main');
+// let gameBody = document.selectElementByClass('.game-body');
+// let section = document.querySelectorAll('section');
 
-        
 //Give background height
 main.style.height = '100vh';
 //Append Div to document
 document.body.appendChild(main);
+// document.body.appendChild(section);
+
 
 //Event Listener for  mouse movement
 main.addEventListener('mousemove', function() {
-    console.log(event);
-    var x1 = event.clientX;
-    var y1 = event.clientY;
+    // console.log(event);
+    let x1 = event.clientX;
+    let y1 = event.clientY;
 
-    var x2 = event.screenX;
-    var y2 = event.screenY;
+    let x2 = event.screenX;
+    let y2 = event.screenY;
 
-    // var gradientDirection = "top right";
+    // let gradientDirection = "top right";
 
     if(x2 < 0) {
         x2 = x2  * (-1);
     }
 
-    let gradientDirection = ( x2 / 3) + "deg" ;
-
-
-    // if (x2 < 400 && y2 < 400) {
-    //     gradientDirection = "top left";
-    // } else if (x2 < 400 && y2 < 400) {
-    //     gradientDirection = "bottom left";
-    // } else if (x2 > 400 && y2 > 400) {
-    //     gradientDirection = "bottom right";
-    // }
+    let gradientDirection = (Math.abs( x2 - y2) / 2) + "deg" ;
     
-    console.log("x: " + x2);
-    console.log("y: " + y2);
+    // console.log("x: " + x2);
+    // console.log("y: " + y2);
 
 
 
     main.style.background = "linear-gradient( " + gradientDirection + ", rgb(" + x1/3 + "," + y1/7 + ",27), rgb(" + x2/5 + "," + y2/4 + ",200))";
+    // section.style.background = "linear-gradient( " + gradientDirection + ", rgb(" + x1/3 + "," + y1/7 + ",27), rgb(" + x2/5 + "," + y2/4 + ",200))";
 })
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,69 @@
+/******************************************
+ * ----------VARIABLES--------------- * 
+ *********************************************/
+
+//Function Expressions
+
+const game = function(wordToGuess, userWord, typeBox) {
+
+
+    // typeBox.addEventListener('click', function() {
+    //     let guessRemaining = document.body.querySelector('.guessCounter').textContent;
+    //     guessRemaining = guessRemaining - 1;
+    //     console.log(guessRemaining);
+    // });
+
+    let guessRemaining = 10;
+
+    $(typeBox).click(function() { 
+        guessRemaining--;
+        console.log(guessRemaining);
+        // $('.guessCounter').append(guessRemaining);
+
+        $('.guessCounter').text(guessRemaining);
+    });
+
+    $('body').keyup(function (e) { 
+        const key = event.key; // "a", "1", "Shift", etc.
+        // console.log(key);
+
+        checkLetterMatch(key, wordToGuess, userWord);
+    });
+
+};
+
+// let guessReducer = function(guessCount) {
+//     guessCount--;
+//     console.log(guessCount);
+//     guessCount--;
+//     console.log(guessCount);
+
+//     // return true;
+// };
+
+let randomizer = function(list) {
+
+    randomWord = list[Math.floor(Math.random() * list.length)];
+    return randomWord;
+};
+
+let underScoreCount = function(charCount) {
+    let underscores = "";
+
+    for (let i = 0; i < charCount; i++) {
+        underscores += "_";
+    }
+    return underscores;
+}
+
+let setColor = function(color, image) {
+    color = color.toLowerCase();
+    image.style.backgroundColor = color;
+}
+
+
+//Essential Variables
+
 const colorList = [
     "Firebrick",
     "Chocolate",
@@ -27,7 +93,7 @@ const colorList = [
     "Aquamarine",
     "Blue",
     "Navy",
-    "Conrsilk",    
+    "Cornsilk",    
     "Brown",
     "Bisque",
     "Wheat",
@@ -40,3 +106,25 @@ const colorList = [
     "Silver",
 ];
 
+let colorMatch = document.body.querySelector('#colorExample');
+let guessArea = document.body.querySelector(".guessArea");
+let wordGenerated = randomizer(colorList);
+let letterCount = wordGenerated.length;
+let wordTranslatedUnderscore = underScoreCount(letterCount);
+let wordOutput = document.createTextNode(wordTranslatedUnderscore);
+
+
+/******************************************
+ * ----------GAME SET UP--------------- * 
+ *********************************************/
+
+setColor(wordGenerated, colorMatch);
+console.log(letterCount + " " + wordGenerated);
+guessArea.appendChild(wordOutput);
+
+
+/******************************************
+ * ----------GAME--------------- * 
+ *********************************************/
+
+ game(wordGenerated, wordOutput, document.body.querySelector(".game-body"));
